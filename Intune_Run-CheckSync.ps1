ececdb7d137e4f846cc861dccfe8d8dff277405e
dIrK <CodeBrah@outlook.de>
2025-10-08 10:45:15 +0200

    Intune_Run-CheckSync.ps1
    .SYNOPSIS
    Intune | Run-CheckSync.ps1
    Created: 9/2025 DPS
    
    .DESCRIPTION
    Checks for event ID associated with Intune MDM sync. Event ID 209 is read in a loop. The sleep timer can be adjusted below.
    
    .EXAMPLE
    PS> .\Run-CheckSync.ps

---
+++ Intune_Run-CheckSync.ps1
@@ -0,0 +1,31 @@
+﻿
+<#
+
+.SYNOPSIS
+Intune | Run-CheckSync.ps1
+Erstellt: 9/2025 DPS
+
+.DESCRIPTION
+Prüft auf Event-ID zum Intune MDM-Sync. Event-ID 209 wird in einer Schleife ausgelesen. Sleep-Timer kann unten angepasst werden. 
+
+.EXAMPLE
+PS> .\Run-CheckSync.ps1
+
+#>
+
+
+$start = Get-Date
+$duration = [TimeSpan]::FromHours(1)
+
+Write-Host "Skript bleibt bis zum manuellen Abbruch in der Schleife" -ForegroundColor Red -BackgroundColor Yellow
+Write-Host "Lese Events.." -ForegroundColor Red -BackgroundColor Yellow
+
+while ((Get-Date) -lt $start.Add($duration)) {
+    Get-WinEvent -MaxEvents 5 -FilterHashtable @{
+        LogName = 'Microsoft-Windows-DeviceManagement-Enterprise-Diagnostics-Provider/Sync'
+        Id      = 209
+    }
+	
+    Start-Sleep -Seconds 15
+    Write-Host "-----------------" + $myDatum(Get-Date)
+}
